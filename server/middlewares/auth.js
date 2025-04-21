d640eb174c34ab36bd843689a59f886b5b87d3de
bhushan <pandagrebhushan3@gmail.com>
2025-04-21 17:50:51 +0530

    bhushan

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,31 +1,14 @@
-// import { User } from "../models/userSchema.js";
-// import { catchAsyncErrors } from "./catchAsyncErrors.js";
-// import ErrorHandler from "./error.js";
-// import jwt from "jsonwebtoken";
-
-// export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-//   const { token } = req.cookies;
-//   if (!token) {
-//     return next(new ErrorHandler("User not Authenticated!", 400));
-//   }
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-//   req.user = await User.findById(decoded.id);
-//   next();
-// });
-
-import jwt from "jsonwebtoken";
 import { User } from "../models/userSchema.js";
-import ErrorHandler from "./error.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
-
   if (!token) {
-    return next(new ErrorHandler("User not authenticated!", 401));
+    return next(new ErrorHandler("User not Authenticated!", 400));
   }
-
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
   next();
 });
